3c6623cd0fef152c8ece78f0a807fb86b771fbd3
Tyler Croley <tjcroley@outlook.com>
2020-05-05 22:00:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   create_table :costume_stores do |x|
-    x.string
-  
+    x.string :name 
+    x.string :location
+    x.integer :costume_inventory
+    x.integer :num_of_employees
+    x.boolean :still_in_business
+    x.datetime :opening_time
+    x.datetime :closing_time
+    end
+  end
 end
